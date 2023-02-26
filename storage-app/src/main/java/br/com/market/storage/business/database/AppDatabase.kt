f2fcdd70d95ee0cb46a2540f6f1d9d69aa6f123c
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2023-02-26 11:55:12 -0300

    fix: ajustando versão do banco

--- storage-app/src/main/java/br/com/market/storage/business/database/AppDatabase.kt
+++ storage-app/src/main/java/br/com/market/storage/business/database/AppDatabase.kt
@@ -9,7 +9,7 @@ import br.com.market.storage.business.models.Product
 import br.com.market.storage.business.models.ProductBrand
 
 @Database(
-    version = 4,
+    version = 5,
     entities = [Product::class, Brand::class, ProductBrand::class],
     exportSchema = true
 )
