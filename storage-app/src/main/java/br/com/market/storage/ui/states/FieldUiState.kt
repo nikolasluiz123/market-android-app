7ab32e442f649b62282bc28002f5597a7ed2028f
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2023-03-04 21:20:04 -0300

    refator: utilização da classe base UIState no Login

--- storage-app/src/main/java/br/com/market/storage/ui/states/FieldUiState.kt
+++
@@ -1,9 +0,0 @@
-package br.com.market.storage.ui.states
-
-data class FieldUiState(
-    val value: String = "",
-    val errorMessage: String = "",
-    val onValueChange: () -> Unit = { }
-) {
-    fun isNotBlankValue() = value.isNotBlank()
-}
--- storage-app/src/main/java/br/com/market/storage/ui/states/LoginUiState.kt
+++ storage-app/src/main/java/br/com/market/storage/ui/states/LoginUiState.kt
@@ -3,14 +3,14 @@ package br.com.market.storage.ui.states
 data class LoginUiState(
     val email: String = "",
     val password: String = "",
-    val serverError: String = "",
-    val showErrorDialog: Boolean = false,
-    val showLoading: Boolean = false,
     val emailErrorMessage: String = "",
     val passwordErrorMessage: String = "",
     val onEmailChange: (String) -> Unit = {},
     val onPasswordChange: (String) -> Unit = {},
-    val onValidate: () -> Boolean = { false },
-    val onToggleErrorDialog: (String) -> Unit = {  },
-    val onToggleLoading: () -> Unit = { }
-)
+    override val serverError: String = "",
+    override val showErrorDialog: Boolean = false,
+    override val onToggleErrorDialog: (String) -> Unit = { },
+    override val showLoading: Boolean = false,
+    override val onToggleLoading: () -> Unit = { },
+    override val onValidate: () -> Boolean = { false }
+) : BaseUiState()
