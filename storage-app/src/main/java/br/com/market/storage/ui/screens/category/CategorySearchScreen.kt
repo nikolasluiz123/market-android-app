17a45eb01f6f9afa16e8392eee8a51faf75ba62e
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2023-04-11 18:23:11 -0300

    refactor: renomeando arquivos das telas de categoria

---
+++ storage-app/src/main/java/br/com/market/storage/ui/screens/category/CategorySearchScreen.kt
@@ -0,0 +1,215 @@
+package br.com.market.storage.ui.screens.category
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import br.com.market.core.theme.BLUE_600
+import br.com.market.core.theme.GREY_800
+import br.com.market.core.theme.MarketTheme
+import br.com.market.core.ui.components.LazyVerticalListComponent
+import br.com.market.core.ui.components.MarketBottomAppBar
+import br.com.market.core.ui.components.MarketCircularBlockUIProgressIndicator
+import br.com.market.core.ui.components.SimpleMarketTopAppBar
+import br.com.market.core.ui.components.buttons.FloatingActionButtonAdd
+import br.com.market.core.ui.components.buttons.IconButtonDelete
+import br.com.market.core.ui.components.buttons.IconButtonLogout
+import br.com.market.storage.R
+import br.com.market.storage.sampledata.sampleCategories
+import br.com.market.storage.ui.states.CategorySearchUIState
+import br.com.market.storage.ui.viewmodels.CategorySearchViewModel
+
+@Composable
+fun CategorySearchScreen(
+    viewModel: CategorySearchViewModel,
+    onButtonBackClickFailureScreen: () -> Unit,
+) {
+    val state by viewModel.uiState.collectAsState()
+    CategorySearchScreen(
+        state = state,
+        onButtonBackClickFailureScreen = onButtonBackClickFailureScreen,
+        onButtonRetryClick = {
+            viewModel.findCategories()
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CategorySearchScreen(
+    state: CategorySearchUIState,
+    onButtonBackClickFailureScreen: () -> Unit = { },
+    onButtonRetryClick: () -> Unit = { },
+    onDeleteCategoryClick: () -> Unit = { },
+    onAddCategoryClick: () -> Unit = { }
+) {
+    Scaffold(
+        topBar = {
+            SimpleMarketTopAppBar(
+                title = "Categorias",
+                showNavigationIcon = false,
+                showMenuWithLogout = false,
+                actions = {
+                    IconButtonLogout()
+                }
+            )
+        },
+        bottomBar = {
+            MarketBottomAppBar(
+                actions = {
+                    IconButtonDelete(onClick = onDeleteCategoryClick)
+                },
+                floatingActionButton = {
+                    FloatingActionButtonAdd(onClick = onAddCategoryClick)
+                }
+            )
+        }
+    ) {
+        when (state) {
+            CategorySearchUIState.Failure -> {
+                FailureScreen(
+                    modifier = Modifier.padding(it),
+                    onButtonBackClick = onButtonBackClickFailureScreen,
+                    onButtonRetryClick = onButtonRetryClick
+                )
+            }
+            CategorySearchUIState.Loading -> {
+                MarketCircularBlockUIProgressIndicator(
+                    modifier = Modifier.padding(it),
+                    show = true,
+                    label = "Carregando"
+                )
+            }
+            is CategorySearchUIState.Success -> {
+                ConstraintLayout(modifier = Modifier.padding(it)) {
+                    val (categoriesListRef) = createRefs()
+
+                    LazyVerticalListComponent(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .constrainAs(categoriesListRef) {
+                                linkTo(start = parent.start, end = parent.end, bias = 0F)
+                                linkTo(top = parent.top, bottom = parent.bottom, bias = 0F)
+                            },
+                        items = state.categories,
+                        emptyStateText = stringResource(R.string.category_search_screen_empty_state_text)
+                    ) { categoryDomain ->
+                        CategoryListCard(categoryName = categoryDomain.name)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FailureScreen(
+    onButtonRetryClick: () -> Unit,
+    onButtonBackClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    message: String = "Ocorreu um erro inesperado"
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        ConstraintLayout(Modifier.fillMaxWidth()) {
+            val (messageRef, retryButtonRef, backButtonRef) = createRefs()
+
+            Text(
+                text = message,
+                color = GREY_800,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.constrainAs(messageRef) {
+                    linkTo(start = parent.start, end = parent.end)
+                    top.linkTo(parent.top)
+                }
+            )
+
+            Button(
+                onClick = onButtonRetryClick,
+                colors = ButtonDefaults.buttonColors(containerColor = BLUE_600),
+                modifier = Modifier.constrainAs(retryButtonRef) {
+                    linkTo(start = parent.start, end = parent.end)
+                    top.linkTo(messageRef.bottom, margin = 8.dp)
+                }
+            ) {
+                Text(
+                    text = "Tentar Novamente",
+                    color = Color.White,
+                    style = MaterialTheme.typography.titleSmall
+                )
+            }
+
+            OutlinedButton(
+                onClick = onButtonBackClick,
+                border = BorderStroke(width = ButtonDefaults.outlinedButtonBorder.width, color = BLUE_600),
+                modifier = Modifier.constrainAs(backButtonRef) {
+                    linkTo(start = retryButtonRef.start, end = retryButtonRef.end)
+                    top.linkTo(retryButtonRef.bottom, margin = 8.dp)
+
+                    width = Dimension.fillToConstraints
+                }
+
+            ) {
+                Text(
+                    text = "Voltar",
+                    color = BLUE_600,
+                    style = MaterialTheme.typography.titleSmall
+                )
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun CategoryScreenFailurePreview() {
+    MarketTheme {
+        Surface {
+            CategorySearchScreen(state = CategorySearchUIState.Failure)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CategoryScreenLoadingPreview() {
+    MarketTheme {
+        Surface {
+            CategorySearchScreen(state = CategorySearchUIState.Loading)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CategoryScreenEmptyListPreview() {
+    MarketTheme {
+        Surface {
+            CategorySearchScreen(state = CategorySearchUIState.Success(emptyList()))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CategoryScreenPreview() {
+    MarketTheme {
+        Surface {
+            CategorySearchScreen(state = CategorySearchUIState.Success(sampleCategories))
+        }
+    }
+}
